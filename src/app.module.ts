da3d43c1b1faba8aa46ca4fbcf7c7f7ce8791b61
wangyuan0108 <870147852@qq.com>
2023-03-30 19:02:46 +0800

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 import configuration from './config/index';
+import * as Joi from 'joi';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -8,7 +11,42 @@ import configuration from './config/index';
       cache: true,
       load: [configuration],
       isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+      }),
+      validationOptions: {
+        // 控制是否允许环境变量中未知的键 默认为true
+        allowUnknown: true,
+        // 在遇到第一个错误时就停止验证,如果为false就返回所有错误，默认为false
+        abortEarly: true,
+      },
     }),
+    // 数据库
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'mysql',
+          // 可能不再支持这种方式，entities 将改成接收 实体类的引用
+          //
+          // entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+          autoLoadEntities: true,
+          keepConnectionAlive: true,
+          ...config.get('db.mysql'),
+          // cache: {
+          //   type: 'ioredis',
+          //   ...config.get('redis'),
+          //   alwaysEnabled: true,
+          //   duration: 3 * 1000, // 缓存3s
+          // },
+        } as TypeOrmModuleOptions;
+      },
+    }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
