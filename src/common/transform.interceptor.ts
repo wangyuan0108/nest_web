c273c61e0807e74de467917cd343616025b29aa4
wangyuan0108 <870147852@qq.com>
2023-04-11 07:25:35 +0800

    结果拦截器

---
+++ src/common/transform.interceptor.ts
@@ -0,0 +1,25 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          data,
+          code: 0,
+          msg: '请求成功',
+        };
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 参数验证拦截器
   app.useGlobalPipes(new ValidationPipe());
+  // 返回结果拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 错误过滤器
+
   await app.listen(3000);
 }
 bootstrap();
